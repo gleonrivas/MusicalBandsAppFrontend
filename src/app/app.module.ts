5dab1f17d96a5b85d5bc65d167f10315f359d152
juanma <camachosanchezjm@gmail.com>
2023-03-22 18:52:45 +0100

    Fa Fa Icons importado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './views/components/login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+// Con el import de abajo, podemos traer iconos a la aplicación de forma nativa, sin necesidad de JPG ni nada. Ahí declaramos qué icono queremos.
+import {faHome, faMessage, faBell, faHashtag, faWrench, faPerson, faPlus, faShare, faXmark, faSearch, faHeart, faSignOut} from '@fortawesome/free-solid-svg-icons';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +15,29 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  // Dentro del constructor de abajo, debemos colocar los iconos que hayamos importado arriba.
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faHome,
+      faBell,
+      faWrench,
+      faMessage,
+      faPerson,
+      faPlus,
+      faShare,
+      faXmark,
+      faSearch,
+      faHeart,
+      faSignOut,
+      faHashtag
+    );
+  }
+}
