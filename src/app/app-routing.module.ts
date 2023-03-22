1abce8e5b199331690080710ab47a1fbc048431b
juanma <camachosanchezjm@gmail.com>
2023-03-22 18:38:02 +0100

    Enrutamiento hecho

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from "@angular/router";
+import {LoginComponent} from "./views/components/login/login.component";
+
+
+// Declaración de la rutas, aquí es donde hay que meter el código que sea
+
+const routes: Routes = [
+  // cada ruta va dentro de {}
+  {
+    // path es el enlace que pondremos en el navegador
+    path: 'login',
+    // component hace referencia a componente que vayamos a enrutar, que se debe de importar arriba
+    component: LoginComponent
+  }
+]
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './views/components/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
