ffe157138d8b87bb445f3738462354d1e66c639f
juanma <camachosanchezjm@gmail.com>
2023-04-10 17:49:08 +0200

    Ionic instalado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome
 import {faHome, faMessage, faBell, faHashtag, faWrench, faPerson, faPlus, faShare, faXmark, faSearch, faHeart, faSignOut} from '@fortawesome/free-solid-svg-icons';
 import { NavegacionComponent } from './views/components/navegacion/navegacion.component';
 import {FormsModule} from "@angular/forms";
+import {IonicModule} from "@ionic/angular";
+import {RegistroComponent} from "./views/pages/registro/registro.component";
+
 @NgModule({
   declarations: [
     AppComponent,
