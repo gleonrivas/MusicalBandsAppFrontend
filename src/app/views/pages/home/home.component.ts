b9b3ea095274cc52656abbb41dcda45e8e5cefe5
gonzalo <gleonrivas@safareyes.es>
2023-04-17 17:20:10 +0200

    lógica home .5

--- src/app/views/pages/home/home.component.ts
+++ src/app/views/pages/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HomeService} from "./service/home.service";
+import {HttpHeaders, HttpParams} from "@angular/common/http";
+import {FormationService} from "../../../shared/services/formation.service";
+import {FormationModel} from "../../../shared/models/formation.model";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  public userFormations:FormationModel[]=[]
+  public auth = localStorage.getItem('Authorization');
+
+  constructor(private formationService:FormationService) {
+  }
+
+  ngOnInit(){
+    this.formationService.getUserFormations().subscribe((data)=>{
+      this.userFormations = data;
+      console.log(data);
+    });
+    console.log(localStorage.getItem('Authorization'))
+
+  }
+
 
 }
