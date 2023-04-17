3a8da9417f079cdabf079d8c77aff9025d1f332d
gonzalo <gleonrivas@safareyes.es>
2023-04-17 19:21:01 +0200

    redireccion implementada

--- src/app/views/pages/home/home.component.ts
+++ src/app/views/pages/home/home.component.ts
@@ -3,6 +3,8 @@ import {HomeService} from "./service/home.service";
 import {HttpHeaders, HttpParams} from "@angular/common/http";
 import {FormationService} from "../../../shared/services/formation.service";
 import {FormationModel} from "../../../shared/models/formation.model";
+import {Router} from "@angular/router";
+import {round} from "@popperjs/core/lib/utils/math";
 
 @Component({
   selector: 'app-home',
@@ -14,8 +16,13 @@ export class HomeComponent implements OnInit{
   public userFormations:FormationModel[]=[]
   public auth = localStorage.getItem('Authorization');
 
-  constructor(private formationService:FormationService) {
+  constructor(
+    private formationService:FormationService,
+    private route:Router,
+    ) {
   }
+
+
 
   ngOnInit(){
     this.formationService.getUserFormations().subscribe((data)=>{
@@ -23,10 +30,12 @@ export class HomeComponent implements OnInit{
       console.log(data);
     });
     console.log(localStorage.getItem('Authorization'))
+    this.formationService.getFormation()
   }
 
-  sendFormation(formation:FormationModel){
+  viewFormation(formation:FormationModel){
     this.formationService.setFormation(formation);
+    this.route.navigateByUrl('/formacion');
   }
 
 
