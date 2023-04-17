70ef84af6290940bd9779e4ac9b7f953f4785b25
gonzalo <gleonrivas@safareyes.es>
2023-04-17 17:45:31 +0200

    rest

---
+++ src/app/shared/services/rest.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(private http: HttpClient) {}
+  private getHeaders() {
+    const apiToken = localStorage.getItem("Authorization");
+    const headers = new HttpHeaders();
+    return headers.set("Authorization", apiToken!);
+  }
+
+  public get<T = Object>(url: string) {
+    return this.http.get<T>(url, {
+      headers: this.getHeaders()
+    });
+  }
+
+  public post<K, T = Object>(url: string, body: K) {
+    return this.http.post<T>(url, body, {
+      headers: this.getHeaders()
+    });
+  }
+
+}
