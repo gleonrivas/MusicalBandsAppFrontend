a41b99a4205e3a5fd8f52f8a98a6806a89fd9735
gonzalo <gleonrivas@safareyes.es>
2023-05-09 18:13:15 +0200

    repertory Type

---
+++ src/app/shared/models/repertoryType.model.ts
@@ -0,0 +1,10 @@
+import {FormationType} from "./formationType.model";
+
+export type RepertoryType = {
+  id?:number,
+  active?:boolean,
+  name?:string,
+  description?:string,
+  formation?:FormationType
+
+}
