5463eb81f2936951324608d19102a9aa60e70f3c
gonzalo <gleonrivas@safareyes.es>
2023-05-09 18:14:28 +0200

    formation service improved with SessionStorage

--- src/app/shared/services/formation.service.ts
+++ src/app/shared/services/formation.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {RestService} from "./rest.service";
 import {FormationType} from "../models/formationType.model";
+import {EnumFormationType} from "../models/Enum/EnumFormationType";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +10,24 @@ import {FormationType} from "../models/formationType.model";
 export class FormationService {
 
   public apikey = localStorage.getItem('Authorization')
-  constructor(private rest: RestService ) { }
+  constructor(private rest: RestService) { }
 
   private formation!:FormationType;
 
   setFormation(formation: FormationType) {
     this.formation = formation;
+    sessionStorage.setItem('idFormation', formation.id)
   }
-  getFormation(): FormationType {
-    return this.formation;
+  getFormation(): Observable<FormationType> {
+    return this.getFormationById(parseInt(sessionStorage.getItem('idFormation')!));
   }
 
   formationFinder(formationName:string){
-    return this.rest.get<FormationType[]>('http://localhost:8080/formation/listByUser');
+    return this.rest.get<FormationType[]>('http://localhost:8080/formation/listByUser'+formationName);
+  }
+
+  getFormationById(formationID:number){
+    return this.rest.get<FormationType>('http://localhost:8080/formation/listById/'+formationID);
   }
 
   getUserFormationsByOwner(){
