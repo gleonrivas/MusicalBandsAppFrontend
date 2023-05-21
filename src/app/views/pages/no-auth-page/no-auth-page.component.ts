a73d2c427fc92b5f5e8b64889bdbb76a9846c415
gonzalo <gleonrivas@safareyes.es>
2023-05-21 18:39:08 +0200

    AUTHENTICATION VALIDATION page

---
+++ src/app/views/pages/no-auth-page/no-auth-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-auth-page',
+  templateUrl: './no-auth-page.component.html',
+  styleUrls: ['./no-auth-page.component.css']
+})
+export class NoAuthPageComponent {
+
+}
